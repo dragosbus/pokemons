b7ee3bc16c0a78bb7ea522c2006613e598dce288
dragosbus <dragosbusuioc94@gmail.com>
2018-08-04 13:45:46 +0200

    created pagination component and styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import PokeList from './components/PokeList';
+import Pagination from './components/Pagination';
+
 const baseURL = 'https://pokeapi.co/api/v2';
 
 class App extends Component {
@@ -15,22 +17,29 @@ class App extends Component {
   }
 
   fetchData(url) {
-    return fetch(url).then(res=>res.json());
+    return fetch(url).then(res=>res.json())
+    .then(res=>{
+      let pages = Math.round(res.count / this.state.limit)
+      this.setState(prevState=>({
+        pokemons: [...prevState.pokemons, ...res.results],
+        totalPages: prevState.totalPages + pages
+      }));
+    })
   }
 
   componentDidMount() {
-    this.fetchData(`${baseURL}/pokemon/?limit=${this.state.limit}&offset=0`)
-      .then(res=>{
-        this.setState(prevState=>({
-          pokemons: [...prevState.pokemons, ...res.results]
-        }));
-      })
+    this.fetchData(`${baseURL}/pokemon/?limit=${this.state.limit}&offset=0`);
   }
 
   render() {
     return(
       <div>
         <PokeList pokemons={this.state.pokemons}/>
+        <Pagination
+          items={this.state.totalPages}
+          activePage={this.state.activePage}
+          onSelect={this.handlePagination}
+        />
       </div>
     );
   }
---
+++ src/components/Pagination.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Pagination = props => {
+    let paginations = [];
+
+    for(let i=0;i<props.items;i++) {
+        paginations.push(i+1);
+    }
+    return(
+        <div className="pagination">
+            {
+                paginations.map(index=> <p key={index}>{index}</p>)
+            }
+        </div>
+    );
+};
+
+export default Pagination;
