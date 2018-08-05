5b50da0e35f4cc3feba3d5aaf091817aaba7e73c
dragosbus <dragosbusuioc94@gmail.com>
2018-08-05 09:47:13 +0200

    save data received from api in localstorage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const baseURL = 'https://pokeapi.co/api/v2';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       pokemons: [],
       activePage: 0,
       limit: 60,
@@ -19,33 +19,45 @@ class App extends Component {
   }
 
   fetchData(url) {
-    return fetch(url).then(res=>res.json())
-    .then(res=>{
-      let pages = this.state.limit / 10;
-      console.log(res)
-      this.setState(prevState=>({
-        pokemons: [...prevState.pokemons, ...res.results],
-        totalPages: prevState.totalPages + pages,
-        copy: [...prevState.copy, ...res.results]
-      }));
-    })
+    return fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        let pages = this.state.limit / 10;
+        console.log(res);
+        this.setState(prevState => ({
+          pokemons: [...prevState.pokemons, ...res.results],
+          totalPages: prevState.totalPages + pages,
+          copy: [...prevState.copy, ...res.results]
+        }));
+      });
   }
 
   handlePagination(index) {
     this.setState({
-      activePage: index-1,
-      copy: this.state.pokemons.slice(this.state.activePage, 10*index)
+      activePage: index - 1,
+      copy: this.state.pokemons.slice(this.state.activePage, 10 * index)
     });
   }
 
   componentDidMount() {
-    this.fetchData(`${baseURL}/pokemon/?limit=${this.state.limit}&offset=0`);
+    let serializedData;
+    if (!localStorage.getItem('pokemons')) {
+      serializedData = this.fetchData(`${baseURL}/pokemon/?limit=${this.state.limit}&offset=0`);
+
+      localStorage.setItem('pokemons', JSON.stringify(serializedData));
+    } else {
+      serializedData = localStorage.getItem('pokemons');
+    }
+
+    this.setState({
+      pokemons: JSON.parse(serializedData)
+    });
   }
 
   render() {
-    return(
+    return (
       <div>
-        <PokeList pokemons={this.state.copy}/>
+        <PokeList pokemons={this.state.copy} />
         <Pagination
           items={this.state.totalPages}
           activePage={this.state.activePage}
